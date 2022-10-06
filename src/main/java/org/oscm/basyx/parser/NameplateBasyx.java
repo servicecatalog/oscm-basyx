774c683773e7d9e2b168bfac25cbf1d332eae3d3
Georgia-Anna Farmaki <georgia-anna.farmaki@fujitsu.com>
2022-10-06 15:10:15 +0200

    Added Nameplate class for Basyx.

--- src/main/java/org/oscm/basyx/parser/NameplateBasyx.java
+++ src/main/java/org/oscm/basyx/parser/NameplateBasyx.java
@@ -28,15 +28,6 @@ public class NameplateBasyx {
     Map<String, IProperty> smPropertiesMap = npm.getProperties();
     List<IProperty> lse = new ArrayList<>(smPropertiesMap.values());
 
-    //    prop.getValue() : OK
-    //    prop.getValueType() : OK
-    //    prop.getCategory() : OK
-    //    prop.getIdShort() : OK
-
-    //    prop.getKind(); : not yet supported
-    //    prop.getModelType(); : not yet supported
-    //    prop.getSemanticId(); : not yet supported
-
     if (!lse.isEmpty()) {
       params =
           lse.stream()
