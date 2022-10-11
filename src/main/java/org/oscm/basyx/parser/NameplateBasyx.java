23b13cbd5107c0e4b981d3f9753f2f2b9f7a2e5f
Georgia-Anna Farmaki <georgia-anna.farmaki@fujitsu.com>
2022-10-11 11:41:19 +0200

    Fix #13 - add mandatory property filtering.

--- src/main/java/org/oscm/basyx/parser/NameplateBasyx.java
+++ src/main/java/org/oscm/basyx/parser/NameplateBasyx.java
@@ -9,6 +9,7 @@
 package org.oscm.basyx.parser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -32,6 +33,7 @@ public class NameplateBasyx {
       params =
           lse.stream()
               .filter(NameplateBasyx::include)
+              .filter(NameplateBasyx::isMandatoryProp)
               .map(ServiceParameterBasyx::new)
               .collect(Collectors.toList());
     }
@@ -40,5 +42,13 @@ public class NameplateBasyx {
 
   static boolean include(IProperty smProp) {
     return smProp.getValueType() != null && smProp.getValueType().toString().trim().length() > 0;
+  }
+
+  static boolean isMandatoryProp(IProperty smProp) {
+    String idShort = smProp.getIdShort();
+    return idShort != null
+        ? Arrays.stream(NameplateMandatoryProperties.values())
+            .anyMatch(e -> e.getIdShort().equals(idShort))
+        : false;
   }
 }
