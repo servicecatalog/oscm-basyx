cb111b7db2cd2f025a10de3a187e351e58b15980
Georgia-Anna Farmaki <georgia-anna.farmaki@fujitsu.com>
2022-11-18 11:39:28 +0100

    Fix #1 - Add http error code handling in import service.

--- import_service.sh
+++ import_service.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-source import.sh
-# global variables section
-# SID="12002"
+if [[ -z $OSCM_HOST ]]; then
+  echo "***OSCM_HOST is not set. OSCM_HOST=<fully qualified name of your OSCM server>."
+  exit 1
+fi
+
+TSID=$1
+USER=${2:-$API_USER_KEY}
+PASS=${3:-$API_PASS}
+
+if [[ -z $USER || -z $PASS || -z $1 ]]; then
+  echo "***Missing arguments. Call $0 <AAS_Id> [<User> <Password>]."
+  exit 1
+fi
 
 # Export the json for a MarketableService to a file: new_marketable_service.json and use this as the payload
 curl -ku "$USER:$PASS" -X GET https://$OSCM_HOST:8881/discovery/mservice/json/$TSID -H "accept: application/problem+json" > /tmp/new_marketable_service.json
-
 SERVICE_PAYLOAD=$(cat /tmp/new_marketable_service.json)
-CREATED_SERVICE_JSON=$(curl -ku "$USER:$PASS" -X POST "https://$OSCM_HOST:8081/oscm-rest-api/v1/services" -H  "accept: */*" -H  "Content-Type: application/json" -d "$SERVICE_PAYLOAD")
-# TODO handle error 400 (Invalid technicalServiceId,
-  Invalid JSON request body: "Invalid field parameter: PROVISIONING_SCRIPT is required and must not be empty",
-  Object of class 'SERVICE' already exists with unique business key value'Festo_3S7PM0CP4BD_FROM_REST'"
-}
-# TODO handle error 201 (Successful ID created)
-# TODO add parameters for Nameplate.
+echo $SERVICE_PAYLOAD
 
-Example JSON Response:
-{
-  "createdObjectName": "Festo_3S7PM0CP4BD_FROM_REST",
-  "createdObjectId": "12000"
-}
-# TODO handle successful service_json (Extract createdObjectId, or empty json).
+HTTP_CODE=$(curl -o /tmp/response.txt -w "%{http_code}" -ku "$USER:$PASS" -X POST "https://$OSCM_HOST:8081/oscm-rest-api/v1/services" -H  "accept: */*" -H  "Content-Type: application/json" -d "$SERVICE_PAYLOAD")
 
+if [[ ${HTTP_CODE} -ne 201 ]];then
+    echo "***Creation of Marketable Service failed with HTTP error code: ${HTTP_CODE}."
+    ERROR_DETAILS=$(cat /tmp/response.txt | sed -n 's|.*"errorDetails":"\([^"]*\)".*|\1|p')
+    echo $ERROR_DETAILS
+    exit 1
+else
+    echo "The Marketable Service Creation was successfull.\n"
+    MS_KEY=$(cat /tmp/response.txt | sed -n 's|.*"createdObjectId":"\([^"]*\)".*|\1|p')
+    echo "The created service key is : ${MS_KEY}."
+fi
 
-SERVICE_JSON=$(curl -ku "$USER:$PASS -X GET "https://$OSCM_HOST:8081/oscm-rest-api/v1/services/$SID" -H  "accept: application/json")
-
+rm -f /tmp/response.txt
 rm -f /tmp/new_marketable_service.json
-echo "Imported Marketable Service $SID."
+
+
--- publish_service.sh
+++ publish_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+source create_pricemodel.sh
 MPID="10001"
-curl -ku "$USER:$PASS" -X PUT "https://$OSCM_HOST:8081/oscm-rest-api/v1/marketplaces/$MPID/entries/$SID" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"anonymousVisible\":true,\"visibleInCatalog\":true,\"etag\":1}"
+curl -ku "$USER:$PASS" -X PUT "https://$OSCM_HOST:8081/oscm-rest-api/v1/marketplaces/$MPID/entries/$MSKEY" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"anonymousVisible\":true,\"visibleInCatalog\":true,\"etag\":1}"
